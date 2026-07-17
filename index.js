ac626a4c64d20fa272f35f77f4f055267486e127
rakesh <rakeshsingh03032004@gmail.com>
2026-07-18 00:20:35 +0530

    new

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+const { configDotenv } = require('dotenv');
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -9,6 +12,9 @@ app.get('/', (req, res) => {
 app.get('/twitter' , (req, res) =>{
     res.send('Hello from Twitter!');
 });
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
